d6e3346d07e9100f7c7133285ca1d9c63b54e559
Veltys <veltys@veltys.es>
2018-07-13 21:08:50 +0200

    FIX: Parámetros de los hijos

--- Python/dht11.py
+++ Python/dht11.py
@@ -26,22 +26,22 @@ STATE_DATA_PULL_UP          = 4
 STATE_DATA_PULL_DOWN        = 5
 
 
-import errno                                                                                # Códigos de error
-import os                                                                                   # Funcionalidades varias del sistema operativo
-import sys                                                                                  # Funcionalidades varias del sistema
+import errno                                                                                                # Códigos de error
+import os                                                                                                   # Funcionalidades varias del sistema operativo
+import sys                                                                                                  # Funcionalidades varias del sistema
 
 if DEBUG_REMOTO:
-    import pydevd                                                                           # Depuración remota
+    import pydevd                                                                                           # Depuración remota
 
-import RPi.GPIO as GPIO                                                                     # Acceso a los pines GPIO
+import RPi.GPIO as GPIO                                                                                     # Acceso a los pines GPIO
 
-import comun                                                                                # Funciones comunes a varios sistemas
+import comun                                                                                                # Funciones comunes a varios sistemas
 
-from threading import Thread                                                                # Capacidades multihilo
-from time import sleep                                                                      # Para hacer pausas
+from threading import Thread                                                                                # Capacidades multihilo
+from time import sleep                                                                                      # Para hacer pausas
 
 try:
-    from config import dht11_config as config                                               # Configuración
+    from config import dht11_config as config                                                               # Configuración
 
 except ImportError:
     print('Error: Archivo de configuración no encontrado', file = sys.stderr)
@@ -60,12 +60,12 @@ class dht11(comun.app):
 
         self._argumentos = None
 
-        self._hijos = []                                                                    # Preparación de la lista contenedora de hijos
+        self._hijos = []                                                                                    # Preparación de la lista contenedora de hijos
 
         super().__init__(config, nombre)
 
 
-    @staticmethod                                                                           # Método estático
+    @staticmethod                                                                                           # Método estático
     def _bits_a_bytes(bits):
         ''' Conversor de bits a bytes
         '''
@@ -89,7 +89,7 @@ class dht11(comun.app):
         return bytess
 
 
-    @staticmethod                                                                           # Método estático
+    @staticmethod                                                                                           # Método estático
     def _calcular_bits(longitudes):
         ''' Cáculo de bits dada una longitud
         '''
@@ -104,7 +104,7 @@ class dht11(comun.app):
             if longitud > mas_larga:
                 mas_larga = longitud
 
-        media = mas_corta + (mas_larga - mas_corta) / 2                                     # Se usa la media para determinar si el periodo es largo o corto
+        media = mas_corta + (mas_larga - mas_corta) / 2                                                     # Se usa la media para determinar si el periodo es largo o corto
 
         bits = []
 
@@ -119,7 +119,7 @@ class dht11(comun.app):
         return bits
 
 
-    @staticmethod                                                                           # Método estático
+    @staticmethod                                                                                           # Método estático
     def _calcular_checksum(bytess):
         ''' Cálculo de la suma de control
         '''
@@ -136,21 +136,21 @@ class dht11(comun.app):
         sleep(pausa)
 
 
-    @staticmethod                                                                           # Método estático
+    @staticmethod                                                                                           # Método estático
     def _procesar_datos(datos):
         ''' Procesado de los datos recibidos
         '''
 
         estado = STATE_INIT_PULL_DOWN
 
-        longitudes = []                                                                     # Contendrá las longitudes de los periodos de subida
-        longitud_actual = 0                                                                 # Contendrá la longitud del periodo previo
+        longitudes = []                                                                                     # Contendrá las longitudes de los periodos de subida
+        longitud_actual = 0                                                                                 # Contendrá la longitud del periodo previo
 
         for dato in datos:
             longitud_actual += 1
 
             if estado == STATE_INIT_PULL_DOWN:
-                if dato == GPIO.LOW:                                                        # Bajada inicial
+                if dato == GPIO.LOW:                                                                        # Bajada inicial
                     estado = STATE_INIT_PULL_UP
 
                     continue
@@ -159,7 +159,7 @@ class dht11(comun.app):
                     continue
 
             if estado == STATE_INIT_PULL_UP:
-                if dato == GPIO.HIGH:                                                       # Subida inicial
+                if dato == GPIO.HIGH:                                                                       # Subida inicial
                     estado = STATE_DATA_FIRST_PULL_DOWN
 
                     continue
@@ -168,7 +168,7 @@ class dht11(comun.app):
                     continue
 
             if estado == STATE_DATA_FIRST_PULL_DOWN:
-                if dato == GPIO.LOW:                                                        # Bajada inicial, lo siguiente será una subida
+                if dato == GPIO.LOW:                                                                        # Bajada inicial, lo siguiente será una subida
                     estado = STATE_DATA_PULL_UP
 
                     continue
@@ -177,7 +177,7 @@ class dht11(comun.app):
                     continue
 
             if estado == STATE_DATA_PULL_UP:
-                if dato == GPIO.HIGH:                                                       # Subida, la longitud de ésta estará determinada en función de si viene un 0 o un 1
+                if dato == GPIO.HIGH:                                                                       # Subida, la longitud de ésta estará determinada en función de si viene un 0 o un 1
                     longitud_actual = 0
 
                     estado = STATE_DATA_PULL_DOWN
@@ -188,7 +188,7 @@ class dht11(comun.app):
                     continue
 
             if estado == STATE_DATA_PULL_DOWN:
-                if dato == GPIO.LOW:                                                        # Bajada, almacenaremos la longitud del periodo previo de subida
+                if dato == GPIO.LOW:                                                                        # Bajada, almacenaremos la longitud del periodo previo de subida
                     longitudes.append(longitud_actual)
 
                     estado = STATE_DATA_PULL_UP
@@ -205,9 +205,9 @@ class dht11(comun.app):
         ''' Recogida de los datos
         '''
 
-        continuo = 0                                                                        # Recopilar datos hasta que se encuentre un continuo
+        continuo = 0                                                                                        # Recopilar datos hasta que se encuentre un continuo
         datos = []
-        max_continuo = 100                                                                  # Usado para determinar el final de los datos
+        max_continuo = 100                                                                                  # Usado para determinar el final de los datos
         ultimo = -1
 
 
@@ -252,16 +252,16 @@ class dht11(comun.app):
             argumentos = self.procesar_argumentos(self._argumentos)
 
             # TODO: Optimizar
-            for i in range(len(self._config.GPIOS)):                                        # Se recorre de dos en dos la lista de puertos GPIO para ir generando los hijos
+            for i in range(len(self._config.GPIOS)):                                                        # Se recorre de dos en dos la lista de puertos GPIO para ir generando los hijos
                 if DEBUG:
                     print('Padre #', os.getpid(), "\tPreparando hijo ", i, sep = '')
 
-                self._hijos.append(Thread(target = main_hijos, args = (i, argumentos,)))    #     Se prepara cada hijo y se configura
+                self._hijos.append(Thread(target = main_hijos, args = ((i, argumentos, self._config),)))    #     Se prepara cada hijo y se configura
 
                 if DEBUG:
                     print('Padre #', os.getpid(), "\tArrancando hijo ", i, sep = '')
 
-                self._hijos[i].start()                                                      #     Se inicia cada hijo
+                self._hijos[i].start()                                                                      #     Se inicia cada hijo
 
         else:
             print('Uso:', argumentos[0], '''[opciones]
@@ -283,13 +283,13 @@ class dht11(comun.app):
         ''' Realiza las operaciones necesarias para el cierre del sistema
         '''
 
-        for hijo in self._hijos:                                                            # Se recorren los hijos
-            hijo.join()                                                                     #     Para esperar su finalización
+        for hijo in self._hijos:                                                                            # Se recorren los hijos
+            hijo.join()                                                                                     #     Para esperar su finalización
 
-        super().cerrar()                                                                    # Llamada al método cerrar() de la clase padre
+        super().cerrar()                                                                                    # Llamada al método cerrar() de la clase padre
 
 
-    @staticmethod                                                                           # Método estático
+    @staticmethod                                                                                           # Método estático
     def procesar_argumentos(argumentos):
         ''' Procesado de los argumentos
         '''
@@ -338,7 +338,7 @@ class dht11_hijos(comun.app):
             - Carga la configuración
         '''
 
-        # super().__init__()                                                                # La llamada al constructor de la clase padre está desactivada a propósito
+        # super().__init__()                                                                                # La llamada al constructor de la clase padre está desactivada a propósito
 
         self._argumentos = argumentos
         self._config = config
@@ -363,27 +363,27 @@ class dht11_hijos(comun.app):
         ''' Lee datos del sensor
         '''
 
-        GPIO.setup(config.GPIOS[self._sensor][0], GPIO.OUT)                                 # Modo de escritura
+        GPIO.setup(config.GPIOS[self._sensor][0], GPIO.OUT)                                                 # Modo de escritura
 
-        self._enviar_y_esperar(GPIO.HIGH, 0.05)                                             # Señal de lectura
+        self._enviar_y_esperar(GPIO.HIGH, 0.05)                                                             # Señal de lectura
 
-        self._enviar_y_esperar(GPIO.LOW, 0.02)                                              # Fin de la señal de lectura
+        self._enviar_y_esperar(GPIO.LOW, 0.02)                                                              # Fin de la señal de lectura
 
-        GPIO.setup(config.GPIOS[self._sensor][0], GPIO.IN, GPIO.PUD_UP)                     # Cambiando a modo lectura
+        GPIO.setup(config.GPIOS[self._sensor][0], GPIO.IN, GPIO.PUD_UP)                                     # Cambiando a modo lectura
 
-        datos = self._recoger_datos()                                                       # Recibiendo datos
+        datos = self._recoger_datos()                                                                       # Recibiendo datos
 
-        longitudes = self._procesar_datos(datos)                                            # Procesamiento de datos
+        longitudes = self._procesar_datos(datos)                                                            # Procesamiento de datos
 
-        if len(longitudes) != LONGITUD_DATOS:                                               # Si no coincide la congitud con el valor esperado, ha habido un fallo en la transmisión
+        if len(longitudes) != LONGITUD_DATOS:                                                               # Si no coincide la congitud con el valor esperado, ha habido un fallo en la transmisión
             return resultado_dht11(ERR_MISSING_DATA, 0, 0)
 
         else:
-            bits = self._calcular_bits(longitudes)                                          # Calcular bits a partir de las longitudes
+            bits = self._calcular_bits(longitudes)                                                          # Calcular bits a partir de las longitudes
 
-            bytess = self._bits_a_bytes(bits)                                               # Calcular bytes
+            bytess = self._bits_a_bytes(bits)                                                               # Calcular bytes
 
-            checksum = self._calcular_checksum(bytess)                                      # Calcular comprobación y comprobar
+            checksum = self._calcular_checksum(bytess)                                                      # Calcular comprobación y comprobar
 
             if bytess[4] != checksum:
                 return resultado_dht11(ERR_CRC, 0, 0)
@@ -418,7 +418,7 @@ class dht11_hijos(comun.app):
                 j = j + 1
 
         if DEBUG_SENSOR or resultado.valido():
-            if self._argumentos[0]:                                                         # Información del sensor
+            if self._argumentos[0]:                                                                         # Información del sensor
                 print('Sensor', self._id_hijo, '-> ', end = '')
 
             if self._argumentos[0] and not self._argumentos[1] and not self._argumentos[2]:
@@ -430,10 +430,10 @@ class dht11_hijos(comun.app):
             elif self._argumentos[1]:
                 print('T', end = '')
 
-            if self._argumentos[1]:                                                         # Temperatura
+            if self._argumentos[1]:                                                                         # Temperatura
                 print('emperatura:', resultado.temperatura, end = '')
 
-            if self._argumentos[1] and self._argumentos[3]:                                 # Unidades
+            if self._argumentos[1] and self._argumentos[3]:                                                 # Unidades
                 print('º C', end = '')
 
             if self._argumentos[1] and self._argumentos[2]:
@@ -445,7 +445,7 @@ class dht11_hijos(comun.app):
             elif self._argumentos[2]:
                 print('H', end = '')
 
-            if self._argumentos[2]:                                                         # Humedad
+            if self._argumentos[2]:                                                                         # Humedad
                 print('umedad relativa:', resultado.humedad, end = '')
 
             if self._argumentos[2] and self._argumentos[3]:
@@ -466,10 +466,10 @@ class dht11_hijos(comun.app):
         if DEBUG:
             print('Hijo  #', self._id_hijo, "\tDisparado el evento de cierre", sep = '')
 
-        # super().cerrar()                                                                  # La llamada al método de cierre de la clase padre está desactivada a propósito
+        # super().cerrar()                                                                                  # La llamada al método de cierre de la clase padre está desactivada a propósito
 
 
-class resultado_dht11:                                                          	        # Clase resultado devuelto por el método dht11.leer()
+class resultado_dht11:                                                          	                        # Clase resultado devuelto por el método dht11.leer()
     error = ERR_NO_ERROR
     temperatura = -1
     humedad = -1
@@ -501,7 +501,7 @@ def main(argv):
 
 
 def main_hijos(argv):
-    app = dht11_hijos(argv, config)
+    app = dht11_hijos(argv[0], argv[1], argv[2])
 
     err = app.arranque()
 
