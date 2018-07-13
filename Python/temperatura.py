278f63ce57a93c624434929807d5e30ffd65811c
Veltys <veltys@veltys.es>
2018-07-13 12:43:01 +0200

    FIX: Vuelta al iterador de range(len(variable)) a la espera de optimizar

--- Python/temperatura.py
+++ Python/temperatura.py
@@ -80,9 +80,9 @@ class temperatura(comun.app):
                     else:                                                                   #         Está igual o por encima del valor máximo
                         j = 3                                                               #             Se asigna la coordenada corespondiente para el posterior acceso a la lista de colores de los leds
 
-                    for led in leds:                                                        #         Se recorre la lista de leds
-                        led.ChangeDutyCycle(self._config.COLORES[j][i] * 100)               #             Se cambia el ciclo de ejecución en función de la cordenada anteriormente asignada
-
+                    # TODO: Optimizar
+                    for i in range(len(leds)):                                              #         Se recorre la lista de leds
+                        leds[i].ChangeDutyCycle(self._config.COLORES[j][i] * 100)           #             Se cambia el ciclo de ejecución en función de la cordenada anteriormente asignada
 
                 sleep(self._config.PAUSA)                                                   #     Pausa hasta la nueva comprobación
 
