3468f6f07b17a88790b25b63d0e19d0737ecd4f2
Veltys <veltys@veltys.es>
2019-04-19 17:03:05 +0200

    ADD: Observador

--- Python/pid.py
+++ Python/pid.py
@@ -5,8 +5,8 @@
 # Title         : pid.py
 # Description   : Módulo auxiliar para ciertas funciones de bloqueo y de PIDs
 # Author        : Veltys
-# Date          : 12-07-2018
-# Version       : 1.0.0
+# Date          : 11-03-2019
+# Version       : 1.0.1
 # Usage         : import pid | from pid import <clase>
 # Notes         : ...
 
@@ -186,3 +186,8 @@ class pid(_comun_pid):
             self._activado = False                                                          #     Sea como fuere, se actualiza la variable interna de información de activación
 
 
+    def pid(self):
+        ''' Observador de la variable "_pid" de la clase
+        '''
+
+        return self._pid
